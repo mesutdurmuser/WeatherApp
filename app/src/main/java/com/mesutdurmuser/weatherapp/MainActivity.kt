01615b85985d664fe1da692b8c7ed99e47cc93e3
Mesut Durmuşer <53917840+mesutdurmuser@users.noreply.github.com>
2022-01-20 20:33:34 +0300

    fix project

--- app/src/main/java/com/mesutdurmuser/weatherapp/MainActivity.kt
+++ app/src/main/java/com/mesutdurmuser/weatherapp/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.mesutdurmuser.weatherapp
 
-import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.activity.viewModels
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.activity.viewModels
 import com.mesutdurmuser.weatherapp.databinding.ActivityMainBinding
 import com.mesutdurmuser.weatherapp.viewmodel.WeatherViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONObject
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel:WeatherViewModel by viewModels()
+    private val viewModel: WeatherViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -26,5 +34,4 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
-
 }
